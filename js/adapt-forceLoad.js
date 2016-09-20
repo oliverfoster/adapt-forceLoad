71bf2b8f751dd00fe7ac02eaf52dfe6eab2eb5eb
Oliver Foster <oliver.foster@kineo.com>
2016-09-20 10:05:47 +0100

    issue/2: click to disable at runtime
    allow forceload to be disabled at runtime

--- js/adapt-forceLoad.js
+++ js/adapt-forceLoad.js
@@ -3,10 +3,16 @@ define([ "coreJS/adapt" ], function(Adapt) {
 	Adapt.once("app:dataReady", function() {
 		if (!Adapt.config.get("_forceLoad")) return;
 
+		var originalTimeoutValue = $.fn.imageready.timeoutDuration;
+
 		$("<div/>")
 			.addClass("force-load")
 			.text("Force Load enabled")
-			.appendTo($("#wrapper"));
+			.appendTo($("#wrapper"))
+			.on("click", function() {
+				$.fn.imageready.timeoutDuration = originalTimeoutValue;
+				this.remove();
+			});
 
 		$.fn.imageready.timeoutDuration = 250;
 
